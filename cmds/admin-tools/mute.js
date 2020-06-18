0ccbfb45d154a40f51db225d40304e3dc9520273
Ravenwulf <marcello.v.pezzella@gmail.com>
2020-06-18 15:36:41 -0500

    Fixed mute command

--- cmds/admin-tools/mute.js
+++ cmds/admin-tools/mute.js
@@ -3,57 +3,58 @@ const config = module.require("../../config.json");
 const ms = module.require('ms')
 module.exports.run = async (bot, msg, args) => {
 
-    if(!msg.member.hasPermission("MUTE_MEMBERS")) return require('../../util/errMsg.js').run(bot, msg, false, "You do not have proper premissions.");
+    if(!msg.member.hasPermission("MANAGE_MESSAGES")) return require('../../util/errMsg.js').run(bot, msg, false, "You do not have proper premissions.");
 
     let USER = msg.mentions.members.first();
     if(!USER) return require('../../util/errMsg.js').run(bot, msg, true, "Please mention a user");
     if(USER.id == msg.author.id) return require('../../util/errMsg.js').run(bot, msg, false, "You cannot mute youself!");
     if(USER.roles.highest.position >= msg.member.roles.highest.position) return require('../../util/errMsg.js').run(bot, msg, false, "Target user is of higher ranking then you!");
-    let ROLE = msg.guild.roles.cache.find(r => r.name === "Muted")
+    let ROLE = msg.guild.roles.cache.find(r => r.name === "Node Muted")
 
     if(!ROLE){
         try {
             ROLE = await msg.guild.roles.create({
                 data: {
-                    name: 'Muted',
+                    name: 'Node Muted',
                     color: 'GREY'
                 },
                 reason: 'Because Muted Role Is Necessary For Command Usage',
             });
+            msg.guild.channels.cache.forEach(async (channel, id) => {
+                await channel.updateOverwrite(ROLE, {
+                    SEND_MESSAGES: false,
+                    ADD_REACTIONS: false,
+                    CONNECT: false
+                });
+            });
         } catch(e) {
             console.log(e.stack);
         }
-        
     }
-    msg.guild.channels.cache.forEach(async (channel, id) => {
-        await channel.updateOverwrite(ROLE, {
-            SEND_MESSAGES: false,
-            ADD_REACTIONS: false,
-            CONNECT: false
-        });
-    });
     if(USER.roles.cache.has(ROLE.id)) return require('../../util/errMsg.js').run(bot, msg, false, `${USER} is already muted!`);
+    let succesembed = new Discord.MessageEmbed({
+        title: `**Mute**`,
+        description: `${USER} was succesfully muted!`,
+        footer: {
+            text: `${msg.author.username}`,
+            icon_url: `${msg.author.displayAvatarURL()}`
+        },
+        timestamp: new Date(),
+        color: (msg.member.displayHexColor)
+    });
     if(!args[1]) {
-    await USER.roles.add(ROLE)
-    msg.channel.send(succesembed)
+        await USER.roles.add(ROLE)
+        msg.channel.send(succesembed)
     }else{
         let role = msg.guild.roles.cache.find(r => r.name === "Muted");
         let time = args[0];
         await USER.roles.add(ROLE);
-        let succesembed = new Discord.MessageEmbed({
-            title: `**Mute**`,
-            description: `${USER} was succesfully muted!`,
-            footer: {
-                text: `${msg.author.username}`,
-                icon_url: `${msg.author.displayAvatarURL()}`
-            },
-            timestamp: new Date(),
-            color: (msg.member.displayHexColor)
-    });
+
         msg.channel.send(succesembed);
-        setTimeout(function(){
-            if(!USER.roles.cache.has(ROLE)) return;
-            user.roles.remove(ROLE);
+        setTimeout(() => {
+            if(!USER.roles.cache.has(ROLE.id)) return;
+            console.log('Made it')
+            USER.roles.remove(ROLE);
             let unmuteembed = new Discord.MessageEmbed({
                 title: `**Unmute`,
                 description: `${USER} has been unmuted!`,
@@ -63,7 +64,7 @@ module.exports.run = async (bot, msg, args) => {
                 },
                 timestamp: new Date(),
                 color: (msg.member.displayHexColor)
-            })
+            });
             msg.channel.send(unmuteembed);
         }, ms(time));
     }
--- cmds/admin-tools/unmute.js
+++ cmds/admin-tools/unmute.js
@@ -3,17 +3,23 @@ const config = module.require("../../config.json");
 const ms = module.require('ms')
 module.exports.run = async (bot, msg, args) => {
 
-    if(!msg.member.hasPermission("MUTE_MEMBERS")) return require('../../util/errMsg.js').run(bot, msg, false, "You do not have proper premissions.");
+    if(!msg.member.hasPermission("MANAGE_MESSAGES")) return require('../../util/errMsg.js').run(bot, msg, false, "You do not have proper premissions.");
     let USER = msg.mentions.members.first();
     if(!USER) return require('../../util/errMsg.js').run(bot, msg, true, "Please mention a user");
-    let ROLE = msg.guild.roles.cache.find(r => r.name === "Muted")
+    let ROLE = msg.guild.roles.cache.find(r => r.name === "Node Muted")
     
-
     USER.roles.remove(ROLE)
-    
-    
-
-
+    let unmuteembed = new Discord.MessageEmbed({
+        title: `**Unmute`,
+        description: `${USER} has been unmuted!`,
+        footer: {
+            text: `${msg.author.username}`,
+            icon_url: `${msg.author.displayAvatarURL()}`
+        },
+        timestamp: new Date(),
+        color: (msg.member.displayHexColor)
+    });
+    msg.channel.send(unmuteembed);
 }
 
 module.exports.help = {
