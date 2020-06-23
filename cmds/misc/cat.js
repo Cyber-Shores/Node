a5367b5e03210791d06fc56cde57993be1118b79
Ravenwulf <marcello.v.pezzella@gmail.com>
2020-06-23 03:00:59 -0500

    Added cat facts to cat command

--- cmds/misc/cat.js
+++ cmds/misc/cat.js
@@ -1,14 +1,31 @@
+const { MessageEmbed } = require('discord.js');
+const { default: Axios } = require('axios');
+
 const snek = module.require('snekfetch');
-const Discord = module.require('discord.js');
 const config = module.require('../../config.json');
-const api = 'http://aws.random.cat/meow';
 module.exports.run = async (bot, msg) => {
 	const m = await msg.channel.send('```Generating image...```');
-	const file = (await snek.get(api)).body.file;
+	const file = (await snek.get('http://aws.random.cat/meow')).body.file;
 	if(!file) return require('../../util/errMsg.js').run(bot, msg, true, 'I broke! Please try again or contact the developer!');
 
-	const attach = await new Discord.MessageAttachment(file, file.split('/').pop());
-	msg.channel.send(attach);
+	// const attach = await new Discord.MessageAttachment(file, file.split('/').pop());
+	let desc = '';
+	await Axios.get('https://catfact.ninja/fact').then(async body => {
+		desc = body.data.fact;
+	});
+
+	msg.channel.send(new MessageEmbed({
+		description: `${desc}\n(image unrelated)`,
+		image: {
+			url: file,
+		},
+		color: msg.member.displayHexColor,
+		timestamp: new Date(),
+		footer: {
+			text: `${msg.author.username}`,
+			icon_url: `${msg.author.displayAvatarURL()}`,
+		},
+	}));
 	m.delete();
 };
 
