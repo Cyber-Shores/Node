06893d43f7aae3b0063e38c121b4aa189b65913d
Washiil <65116802+Washiil@users.noreply.github.com>
2020-06-24 15:29:26 -0700

    removed setprefix from bot.js

--- bot.js
+++ bot.js
@@ -123,29 +123,6 @@ bot.on('message', async msg => {
         return msg.reply(prefixembed);
 
     }
-
-    if(msg.content.includes(`setprefix`)) {
-        if(!args[1] || !args[2]) return require('./util/errMsg').run(bot, msg, true, "Please provide both a prefix and a suffix!");
-        if(args[1].length > 2 || args[2].length > 2) return require('./util/errMsg').run(bot, msg, true, "Neither the prefix nor the suffix can be over 2 charachters long!");
-        //setting a new prefix using the default one
-        if(!msg.member.hasPermission("ADMINISTRATOR")) return require('./util/errMsg').run(bot, msg, false, "You do not have proper premissions.");
-        const req = await GuildModel.findOne({ id: msg.guild.id });
-        if(!req) return msg.reply('Sorry there was an error!');
-        await GuildModel.findOneAndUpdate({ id: msg.guild.id }, { $set: { suffix: `${args[2]}`}}, { new: true});
-        await GuildModel.findOneAndUpdate({ id: msg.guild.id }, { $set: { prefix: `${args[1]}`}}, { new: true});
-        let setprefixembed = await new Discord.MessageEmbed({
-            title: `New Prefix and Suffix`,
-            description: `Prefix: ${args[1]}\nSuffix: ${args[2]}\n`,
-            color: msg.member.displayHexColor,
-        footer: {
-            "text": msg.author.username,
-            "icon_url": msg.author.displayAvatarURL()
-        },
-        timestamp: Date.now()
-        });
-        return await msg.channel.send(setprefixembed)
-        //end
-    }
     //end
 });
 //end
