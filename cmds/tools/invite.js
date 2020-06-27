f3e917da2b39720c4ed8f874662793c9196e936a
Ravenr_ <marcello.v.pezzella@gmail.com>
2020-06-27 09:35:57 -0500

    Changed invite link from admin to all perms

--- cmds/tools/invite.js
+++ cmds/tools/invite.js
@@ -2,7 +2,38 @@ const { MessageEmbed } = module.require('discord.js');
 const config = module.require('../../config.json');
 module.exports.run = async (bot, msg) => {
 	try {
-		const link = await bot.generateInvite(['ADMINISTRATOR']);
+		const link = await bot.generateInvite([
+			'CREATE_INSTANT_INVITE'
+			, 'KICK_MEMBERS'
+			,'BAN_MEMBERS'
+			,'MANAGE_CHANNELS'
+			,'MANAGE_GUILD'
+			,'ADD_REACTIONS'
+			,'VIEW_AUDIT_LOG'
+			,'PRIORITY_SPEAKER'
+			,'STREAM'
+			,'VIEW_CHANNEL'
+			,'SEND_MESSAGES'
+			,'SEND_TTS_MESSAGES'
+			,'MANAGE_MESSAGES'
+			,'EMBED_LINKS'
+			,'ATTACH_FILES'
+			,'READ_MESSAGE_HISTORY'
+			,'MENTION_EVERYONE'
+			,'USE_EXTERNAL_EMOJIS'
+			,'VIEW_GUILD_INSIGHTS'
+			,'CONNECT'
+			,'SPEAK'
+			,'MUTE_MEMBERS'
+			,'DEAFEN_MEMBERS'
+			,'MOVE_MEMBERS'
+			,'USE_VAD'
+			,'CHANGE_NICKNAME'
+			,'MANAGE_NICKNAMES'
+			,'MANAGE_ROLES'
+			,'MANAGE_WEBHOOKS'
+			,'MANAGE_EMOJIS'
+		]);
 		const embed = new MessageEmbed({
 			color: msg.member.displayHexColor,
 			author: {
