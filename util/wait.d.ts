9abefc167e8b73916830605dc5f807875cb68765
Hamziniii <hamzagaziuddin04@gmail.com>
2020-06-30 19:08:35 -0500

    little helper function that emulates sleep with promises

---
+++ util/wait.d.ts
@@ -0,0 +1,10 @@
+/**
+ * Waits for time ms
+ * @param time Time in ms
+ */
+export function run(time: number): Promise<void>
+
+export interface help {
+    name: "wait",
+    description: "resolves after a specific time"
+}
---
+++ util/wait.js
@@ -0,0 +1,8 @@
+module.exports.run = async (time) => new Promise(resolve => {
+    console.log("Waiting for: " + time/1000)
+    setTimeout(() => resolve(time), time)
+})
+module.exports.help = {
+    name: "wait",
+    description: "resolves after a specific time"
+}
