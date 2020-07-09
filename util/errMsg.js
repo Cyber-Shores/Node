8d6c49ada79bbd90027a6e6d792469c0fab7ae4a
Ravenwulf <marcello.v.pezzella@gmail.com>
2020-07-09 14:18:05 -0500

    Fixed eslint problems in errMsg.js

--- util/errMsg.js
+++ util/errMsg.js
@@ -1,25 +1,23 @@
-const Discord = module.require("discord.js");
-const { Client, MessageEmbed } = module.require('discord.js');
-const config = module.require("../config.json");
+const { MessageEmbed } = module.require('discord.js');
 module.exports.run = async (bot, msg, isWarn, text) => {
-    function genWarn(){
-        if(isWarn) return ['⚠️', 'YELLOW'];
-        return ['❌', 'RED'];
-    }
-    let embed = new MessageEmbed({
-        title: "**Error**",
-        description: `${genWarn()[0]} ${text}`,
-        color: genWarn()[1],
-        footer: {
-            "text": msg.author.username,
-            "icon_url": msg.author.displayAvatarURL()
-        },
-        timestamp: Date.now()
-    });
-    msg.channel.send(embed);
-}
+	function genWarn() {
+		if(isWarn) return ['⚠️', 'YELLOW'];
+		return ['❌', 'RED'];
+	}
+	const embed = new MessageEmbed({
+		title: '**Error**',
+		description: `${genWarn()[0]} ${text}`,
+		color: genWarn()[1],
+		footer: {
+			'text': msg.author.username,
+			'icon_url': msg.author.displayAvatarURL(),
+		},
+		timestamp: Date.now(),
+	});
+	msg.channel.send(embed);
+};
 
 module.exports.help = {
-    name: "errMsg",
-    description: "Generates an error message embed"
-}
+	name: 'errMsg',
+	description: 'Generates an error message embed',
+};
