fdb72dcd71ef589463a08b2ba8e495b8a5eba8a1
Ravenwulf <marcello.v.pezzella@gmail.com>
2020-07-09 14:20:59 -0500

    Fixed eslint problems in /util

--- util/suggestMsg.js
+++ util/suggestMsg.js
@@ -1,24 +1,24 @@
-const Discord = module.require("discord.js");
+/* eslint-disable no-unused-vars */
+const Discord = module.require('discord.js');
 const { Client, MessageEmbed } = module.require('discord.js');
-const config = module.require("../config.json");
+const config = module.require('../config.json');
 module.exports.run = async (bot, msg, text, del = false) => {
-    let embed = new MessageEmbed({
-        title: "**Notice**",
-        description: `🔍 ${text}`,
-        color: "#BCBCBC",
-        footer: {
-            "text": msg.author.username,
-            "icon_url": msg.author.displayAvatarURL()
-        },
-        timestamp: Date.now()
-    });
-    let sent = msg.channel.send(embed)
-    if(del)
-        sent.then(_ => _.delete({timeout: 5000}))
-    return sent
-}
+	const embed = new MessageEmbed({
+		title: '**Notice**',
+		description: `🔍 ${text}`,
+		color: '#BCBCBC',
+		footer: {
+			'text': msg.author.username,
+			'icon_url': msg.author.displayAvatarURL(),
+		},
+		timestamp: Date.now(),
+	});
+	const sent = msg.channel.send(embed);
+	if(del) {sent.then(_ => _.delete({ timeout: 5000 }));}
+	return sent;
+};
 // This is useful for suggestions that are not warnings or errors per se -- Hamziniii 🎩
 module.exports.help = {
-    name: "suggestMsg",
-    description: "Generates an suggestion message embed"
-}
+	name: 'suggestMsg',
+	description: 'Generates an suggestion message embed',
+};
--- util/wait.js
+++ util/wait.js
@@ -1,8 +1,8 @@
 module.exports.run = async (time) => new Promise(resolve => {
-    console.log("Waiting for: " + time/1000)
-    setTimeout(() => resolve(time), time)
-})
+	console.log('Waiting for: ' + time / 1000);
+	setTimeout(() => resolve(time), time);
+});
 module.exports.help = {
-    name: "wait",
-    description: "resolves after a specific time"
-}
+	name: 'wait',
+	description: 'resolves after a specific time',
+};
