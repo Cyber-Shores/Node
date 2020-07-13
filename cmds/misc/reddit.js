5db635a8f53a1e04ce74e74d366f35786dff234d
Ravenwulf <marcello.v.pezzella@gmail.com>
2020-07-13 15:55:57 -0500

    misc fixes to reddit command formatting

--- cmds/misc/reddit.js
+++ cmds/misc/reddit.js
@@ -9,9 +9,9 @@ module.exports.run = async (bot, msg, args) => {
 	sf.get(`https://www.reddit.com/r/${args[0]}/random.json?limit=1`).then(res => {
 		if(!res.body) {
 			m.delete();
-			return msg.channel.send(`No Post could be found!`)
+			return msg.channel.send('No Post could be found!');
 		}
-		const url = res.body[0].data.children[0].data.url
+		const url = res.body[0].data.children[0].data.url;
 		// console.log(res.body[0].data.children[0].data.over_18)
 		// console.log(msg.channel.nsfw)
 
@@ -21,30 +21,28 @@ module.exports.run = async (bot, msg, args) => {
 				nsfwembed
 					.setTitle(`Post From r/${args[0]}`)
 					.setColor(msg.author.displayHexColor)
-					.setImage(url)
+					.setImage(url);
 
-				
+
 				m.delete();
-				return msg.channel.send(nsfwembed)
-			}else{
+				return msg.channel.send(nsfwembed);
+			}
+			else{
 				m.delete();
 				return require('../../util/errMsg.js').run(bot, msg, false, 'This channel is not NSFW!');
 			}
 		}
 
 		const sfwembed = new Discord.MessageEmbed();
-				sfwembed
-					.setTitle(`Post From r/${args[0]}`)
-					.setColor(msg.author.displayHexColor)
-					.setImage(url)
+		sfwembed
+			.setTitle(`Post From r/${args[0]}`)
+			.setColor(msg.author.displayHexColor)
+			.setImage(url);
 		m.delete();
-		msg.channel.send(sfwembed)
-		console.log(url)
+		msg.channel.send(sfwembed);
+		console.log(url);
 
-		
-		
-		
-		
+
 	});
 };
 
