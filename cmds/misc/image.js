ad26b74d260cf075280e5497654ac99388bbaa03
Ravenwulf <marcello.v.pezzella@gmail.com>
2020-07-13 16:17:01 -0500

    fixed issue with loading message

--- cmds/misc/image.js
+++ cmds/misc/image.js
@@ -4,6 +4,7 @@ const cheerio = module.require('cheerio');
 const request = module.require('request');
 module.exports.run = async (bot, msg, args) => {
 	if(!args[0]) return require('../../util/errMsg.js').run(bot, msg, true, 'Please provide a search term.');
+	const m = await msg.channel.send('```Generating image...```');
 	const options = {
 		// eslint-disable-next-line no-inline-comments
 		url: encodeURI('http://results.dogpile.com/serp?qc=images&q=' + args.join(' ')), // + args.join('%20'),
@@ -18,8 +19,12 @@ module.exports.run = async (bot, msg, args) => {
 		const $ = cheerio.load(responseBody);
 		const links = $('.image a.link');
 		const urls = new Array(links.length).fill(0).map((v, i) => links.eq(i).attr('href'));
-		if(!urls.length) return require('../../util/errMsg.js').run(bot, msg, false, `No results for "${args.join(' ')}"`);
+		if(!urls.length) {
+			m.delete();
+			return require('../../util/errMsg.js').run(bot, msg, false, `No results for "${args.join(' ')}"`);
+		}
 		const url = urls[Math.floor(Math.random() * urls.length)];
+		m.delete();
 		msg.channel.send(new MessageEmbed({
 			title: args.join(' '),
 			url: url,
