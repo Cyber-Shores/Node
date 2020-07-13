af8730eca1cd3ffa4b71f8b25e296af62e30d555
Ravenwulf <marcello.v.pezzella@gmail.com>
2020-07-13 15:48:58 -0500

    fixed cat command

--- cmds/misc/cat.js
+++ cmds/misc/cat.js
@@ -4,30 +4,30 @@ const { default: Axios } = require('axios');
 const snek = module.require('snekfetch');
 const config = module.require('../../config.json');
 module.exports.run = async (bot, msg) => {
-	return require('../../util/errMsg.js').run(bot, msg, false, 'Known Error! A patch will be release soon!');
-	// const m = await msg.channel.send('```Generating image...```');
-	// const file = (await snek.get('http://aws.random.cat/meow')).body.file;
-	// if(!file) return require('../../util/errMsg.js').run(bot, msg, true, 'I broke! Please try again or contact the developer!');
+	// return require('../../util/errMsg.js').run(bot, msg, false, 'Known Error! A patch will be release soon!');
+	const m = await msg.channel.send('```Generating image...```');
+	const file = (await snek.get('http://aws.random.cat/meow')).body.file;
+	if(!file) return require('../../util/errMsg.js').run(bot, msg, true, 'I broke! Please try again or contact the developer!');
 
-	// // const attach = await new Discord.MessageAttachment(file, file.split('/').pop());
-	// let desc = '';
-	// await Axios.get('https://catfact.ninja/fact').then(async body => {
-	// 	desc = body.data.fact;
-	// });
+	// const attach = await new Discord.MessageAttachment(file, file.split('/').pop());
+	let desc = '';
+	await Axios.get('https://catfact.ninja/fact').then(async body => {
+		desc = body.data.fact;
+	});
 
-	// msg.channel.send(new MessageEmbed({
-	// 	description: `${desc}\n(image unrelated)`,
-	// 	image: {
-	// 		url: file,
-	// 	},
-	// 	color: msg.member.displayHexColor,
-	// 	timestamp: new Date(),
-	// 	footer: {
-	// 		text: `${msg.author.username}`,
-	// 		icon_url: `${msg.author.displayAvatarURL()}`,
-	// 	},
-	// }));
-	// m.delete();
+	msg.channel.send(new MessageEmbed({
+		description: `${desc}\n(image unrelated)`,
+		image: {
+			url: file,
+		},
+		color: msg.member.displayHexColor,
+		timestamp: new Date(),
+		footer: {
+			text: `${msg.author.username}`,
+			icon_url: `${msg.author.displayAvatarURL()}`,
+		},
+	}));
+	m.delete();
 };
 
 module.exports.help = {
