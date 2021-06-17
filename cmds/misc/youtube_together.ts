0d2a57e7bd6132fc20e11c7b4f0d06fdbf6470fa
Hamziniii <hamzagaziuddin04@gmail.com>
2021-06-16 20:58:14 -0500

    Added better directions again

--- cmds/misc/youtube_together.ts
+++ cmds/misc/youtube_together.ts
@@ -39,7 +39,7 @@ module.exports.run = async (bot: Client, msg: Message, args) => {
         //             })
 
 		const channel = msg.member.voice.channel || msg.mentions.channels.first() || msg.guild.channels.cache.get(args[0]);
-        if (!channel || channel.type !== "voice") return msg.channel.send("❌ | Must join a channel first OR Invalid channel specified!");
+        if (!channel || channel.type !== "voice") return msg.channel.send("❌ | Must join a channel first! Directions here: https://imgur.com/Pea0Wi2");
         if (!channel.permissionsFor(msg.guild.me).has("CREATE_INSTANT_INVITE")) return msg.channel.send("❌ | I need `CREATE_INSTANT_INVITE` permission");
 
         fetch(`https://discord.com/api/v8/channels/${channel.id}/invites`, {
